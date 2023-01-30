58a15b6fa2ec1cb21dbf48eab53252b932d83538
gitpichardo <aopichar@gmail.com>
2023-01-30 13:17:04 -0800

    implement Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Routes, Route, useNavigate } from 'react-router-dom'
+
 // Components
+import Nav from './components/Nav'
 import Landing from './pages/Landing/Landing'
 
 const App = () => {
   return (
-    <Landing />
+    <>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/puppies' element={
+          <h1>PUPPY LIST</h1>
+        } />
+      </Routes>
+    </>
   )
 }
 
