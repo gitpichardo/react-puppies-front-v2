d2fd34657ebb15dd286da4446346355ed37a0112
gitpichardo <aopichar@gmail.com>
2023-01-30 15:26:39 -0800

    implement form submit

--- src/pages/NewPuppy/NewPuppy.jsx
+++ src/pages/NewPuppy/NewPuppy.jsx
@@ -2,19 +2,38 @@ import './NewPuppy.css'
 import { useState } from "react"
 
 const NewPuppy = (props) => {
+  const [form, setForm] = useState({
+    name: '',
+    breed: '',
+    age: 0,
+    color: 'Grey',
+    ears: 'Drop'
+  })
+
+  const handleChange = (evt) => {
+    setForm({...form, [evt.target.name]: evt.target.value})
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    props.handleAddPuppy(form)
+  }
+
   return (
     <main className="new">
       <h1>NEW PUPPY</h1>
-      <form autoComplete="off">
+      <form  autoComplete="off" onSubmit={handleSubmit}>
         <label htmlFor="name-input">Name</label>
         <input
           required
           type="text"
           name="name"
           id="name-input"
+          value={form.name}
           placeholder="Name"
+          onChange={handleChange}
         />
-
+  
         <label htmlFor="title-input">Age</label>
         <input
           required
@@ -22,23 +41,29 @@ const NewPuppy = (props) => {
           type="number"
           name="age"
           id="age-input"
+          value={form.age}
           placeholder="Age"
+          onChange={handleChange}
         />
-
+  
         <label htmlFor="breed-input">Breed</label>
         <input
           required
           type="text"
           name="breed"
           id="breed-input"
+          value={form.breed}
           placeholder="Breed"
+          onChange={handleChange}
         />
-
+  
         <label htmlFor="color-input">Color</label>
         <select
           required
           name="color"
           id="color-input"
+          value={form.color}
+          onChange={handleChange}
         >
           <option value="Grey">Grey</option>
           <option value="Black">Black</option>
@@ -46,12 +71,14 @@ const NewPuppy = (props) => {
           <option value="White">White</option>
           <option value="Golden">Golden</option>
         </select>
-
+  
         <label htmlFor="ears-input">Ears</label>
         <select
           required
           name="ears"
           id="ears-input"
+          value={form.ears}
+          onChange={handleChange}
         >
           <option value="Drop">Drop</option>
           <option value="Pointy">Pointy</option>
